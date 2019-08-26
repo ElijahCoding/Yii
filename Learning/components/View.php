97723f25b0a3a1a3a720a89e5d604f117d8786e0
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-27 00:02:26 +0300

    global View

--- Learning/components/View.php
+++ Learning/components/View.php
@@ -2,7 +2,10 @@
 
 namespace app\components;
 
-class View
+class View extends \yii\web\View
 {
-    
+    function render($view, $params = [], $context = null)
+    {
+        return str_ireplace('garlic', 'g#%@c', parent::render($view, $params, $context));
+    }
 }
