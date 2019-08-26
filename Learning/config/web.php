493daebe498ffb5d55276b3d56ded881c1e67d7b
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-26 21:15:06 +0300

    set up

--- Learning/config/web.php
+++ Learning/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'QQ976bJYLTHxmMtiSu4he6xqFzBNc4oO',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
