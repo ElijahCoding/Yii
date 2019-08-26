48efb1ffa88a1972938ca7000501dcc6a2734890
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-27 00:13:05 +0300

    first model

--- Learning/config/db.php
+++ Learning/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=8889;dbname=fresh',
+    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=yii',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
