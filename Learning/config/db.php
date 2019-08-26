b3ba7714228e286f88f9180d50cff3e09fe4152e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-26 23:51:06 +0300

    db connection with port

--- Learning/config/db.php
+++ Learning/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;port=8889;dbname=fresh',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
