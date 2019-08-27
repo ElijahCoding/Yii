521b78bb35152e2c6192f594d4e0602e72fa1b6a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-27 23:14:14 +0300

    views setting

--- Learning/views/monstertest/index.php
+++ Learning/views/monstertest/index.php
@@ -9,6 +9,11 @@ use yii\grid\GridView;
 
 $this->title = 'Monstertests';
 $this->params['breadcrumbs'][] = $this->title;
+$this->registerMetaTag(['name' => 'description', 'content' => 'my first yii application']);
+$this->registerJsFile(
+    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+    ['position' => $this::POS_HEAD]
+);
 ?>
 <div class="monstertest-index">
 
