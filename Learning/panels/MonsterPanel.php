9551df066d38b09ed92e9db72666f4af1c21aa72
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-28 22:33:33 +0300

    debugging with panel

---
+++ Learning/panels/MonsterPanel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\panels;
+
+use yii\base\Event;
+use app\models\MonsterTest;
+use yii\debug\Panel;
+
+class MonsterPanel extends Panel
+{
+    private $_monsters = [];
+
+    public function init()
+    {
+        parent::init();
+        Event::on(
+            MonsterTest::className(),
+            MonsterTest::EVENT_AFTER_FIND,
+            function ($event) {
+                $this->_monsters[] = $event->sender->name . ' was loaded.';
+            }
+        );
+    }
+
+    public function getName()
+    {
+        return 'Monsters';
+    }
+
+    public function getSummary()
+    {
+        $url = $this->getUrl();
+        $count = count($this->data);
+        return "<div class=\"yii-debug-toolbar-block\">
+            <a href=\"$url\">Monsters <span class=\"label\">$count</span></a>
+        </div>";
+    }
+
+    public function getDetail()
+    {
+        return '<ol><li>' . implode('<li>', $this->data) . '</ol>';
+    }
+
+    public function save()
+    {
+        return $this->_monsters;
+    }
+}
