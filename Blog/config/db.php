c701125c1807a4cabaca928df652e1caf85ba691
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-03 16:22:09 +0300

    built

--- Blog/config/db.php
+++ Blog/config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=yii',
+    'dsn' => 'mysql:host=localhost;dbname=treasure',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
--- Blog/models/ArticleTag.php
+++ Blog/models/ArticleTag.php
@@ -7,12 +7,17 @@ use Yii;
 /**
  * This is the model class for table "article_tag".
  *
- * @property int $id
+ * @property integer $id
+ * @property integer $article_id
+ * @property integer $tag_id
+ *
+ * @property Tag $tag
+ * @property Article $article
  */
 class ArticleTag extends \yii\db\ActiveRecord
 {
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public static function tableName()
     {
@@ -20,20 +25,42 @@ class ArticleTag extends \yii\db\ActiveRecord
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function rules()
     {
-        return [];
+        return [
+            [['article_id', 'tag_id'], 'integer'],
+            [['tag_id'], 'exist', 'skipOnError' => true, 'targetClass' => Tag::className(), 'targetAttribute' => ['tag_id' => 'id']],
+            [['article_id'], 'exist', 'skipOnError' => true, 'targetClass' => Article::className(), 'targetAttribute' => ['article_id' => 'id']],
+        ];
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function attributeLabels()
     {
         return [
             'id' => 'ID',
+            'article_id' => 'Article ID',
+            'tag_id' => 'Tag ID',
         ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTag()
+    {
+        return $this->hasOne(Tag::className(), ['id' => 'tag_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticle()
+    {
+        return $this->hasOne(Article::className(), ['id' => 'article_id']);
     }
 }
