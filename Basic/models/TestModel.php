88920f3149df6944913d6c7f5b3c29a6f63233f0
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-03 21:16:28 +0300

    rules

--- Basic/models/TestModel.php
+++ Basic/models/TestModel.php
@@ -9,4 +9,19 @@ class TestModel extends Model
     public $name;
     public $surname;
     public $email;
+
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Enter your name',
+            'age' => 'your age'
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+            [['name', 'surname'], 'required', 'message' => 'please enter you name.']
+        ];
+    }
 }
